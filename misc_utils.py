826cf1691d45bf45b392fb7b3f2eedd61b451d79
prnm2 <mehmetaytimr@gmail.com>
2024-06-26 11:09:20 +0200

    Update misc_utils.py

--- misc_utils.py
+++ misc_utils.py
@@ -3,25 +3,15 @@ import numpy as np
 import torch
 from unidecode import unidecode
 
-
+#This function returns geometric mean of a list
 def g_mean(list_):
     log_list_ = np.log(list_)
     return np.exp(log_list_.mean())
 
 
-def compute_qerrors(actual_card, estimated_card):
-    return max(actual_card/estimated_card, estimated_card/actual_card)
-
-def binary_crossentropy(preds, targets, mask):
-    loss = targets * torch.log(preds + 0.00001) + (1 - targets) * torch.log(1 - (preds - 0.00001))
-    if mask is not None:
-        loss = mask * loss
-    return - torch.sum(loss) / torch.sum(mask)
-
-
+#This function transforms the LIKE patterns to new language
 def LIKE_pattern_to_newLanguage(pattern_list, pattern_type):
     transformed_pattern = ''
-
     for pattern in pattern_list:
         if len(pattern) == 1:
             transformed_pattern += pattern
@@ -34,8 +24,8 @@ def LIKE_pattern_to_newLanguage(pattern_list, pattern_type):
                     count += 1
                 else:
                     if (
-                        new_pattern[-1] not in ('_')
-                        and char not in ('_')
+                        new_pattern[-1] not in ('_', '@')
+                        and char not in ('_', '@')
                     ):
                         new_pattern += char + '$'
                     else:
@@ -54,7 +44,12 @@ def LIKE_pattern_to_newLanguage(pattern_list, pattern_type):
     return transformed_pattern
 
 
-
+#This function computes loss
+def binary_crossentropy(preds, targets, mask):
+    loss = targets * torch.log(preds + 0.00001) + (1 - targets) * torch.log(1 - (preds - 0.00001))
+    if mask is not None:
+        loss = mask * loss
+    return - torch.sum(loss) / torch.sum(mask)
 
 
 def name2tensor(name, char2idx):
@@ -64,21 +59,26 @@ def name2tensor(name, char2idx):
     return tensor
 
 
+#This function loads LIKE-patterns with ground truth probabilities
 def loadtrainData(filename, char2idx):
     inputs = []
     targets = []
     length = []
-    with open(filename, 'r') as file:
-        for line in file:
-            line_ = line.strip().split(':')
-            transformedLikepattern = LIKE_pattern_to_newLanguage(line_[0].split('%'), line_[1])
-            transformed_to_tensor = name2tensor(unidecode(transformedLikepattern), char2idx)
-            inputs.append(transformed_to_tensor)
-            length.append(len(transformed_to_tensor))
-            ground_prob_list = [float(element) for element in line_[-1].split(' ')]
-            targets.append(ground_prob_list)
+    count = 0
+    for line in open(filename):
+        line_ = line.strip().split(':')
+        transformedLikepattern = LIKE_pattern_to_newLanguage(line_[0].split('%'), line_[1])
+        transformed_to_tensor = name2tensor(unidecode(transformedLikepattern), char2idx)
+        inputs.append(transformed_to_tensor)
+        length.append(len(transformed_to_tensor))
+        ground_prob_list = [float(element) for element in line_[-1].split(' ')]
+        targets.append(ground_prob_list)
+        count +=1
+        if count == 300000:
+            break
     return inputs, targets, max(length)
 
+#This function pads the zero vectors to like-patterns.
 def addpaddingTrain(filename, char2idx):
     zeros_vector = [[0] * len(char2idx)]
     padded_inputs = []
@@ -96,10 +96,12 @@ def addpaddingTrain(filename, char2idx):
     return train_dataset
 
 
+#This function takes a file path that contains test LIKE-patterns
 def loadtestData(filename, char2idx):
     inputs = []
     length = []
     actual_card = []
+    count = 0
     with open(filename, 'r') as file:
         for line in file:
             line_ = line.strip().split(':')
@@ -108,6 +110,9 @@ def loadtestData(filename, char2idx):
             transformed_to_tensor = name2tensor(unidecode(transformedLikepattern), char2idx)
             inputs.append(transformed_to_tensor)
             length.append(len(transformed_to_tensor))
+            count +=1
+            if count ==100000:
+                break
     return inputs, max(length), actual_card
 
 
@@ -126,3 +131,7 @@ def addpaddingTest(filename, char2idx):
     test_dataset = [(torch.tensor(padded_inputs[i]), torch.tensor(masks[i]), torch.tensor(actual_card[i])) for i in range(len(masks))]
     return test_dataset
 
+
+##This function compute and return q-error
+def compute_qerrors(actual_card, estimated_card):
+    return max(actual_card/estimated_card, estimated_card/actual_card)
